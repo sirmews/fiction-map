630d9f54ecaced4c65142253fa6b43dd1f69178f
sirmews <sirmews@gmail.com>
2026-06-20 10:37:20 +1000

    feat(protocol): define Frame and Intent protocol types and generate bindings

---
+++ packages/protocol/src/index.ts
@@ -0,0 +1,10 @@
+export type {
+  Frame,
+  FrameChoice,
+  FrameInventoryItem,
+  FrameNode,
+  FrameNodeBlock,
+  FramePacing,
+  Intent,
+} from "./generated/protocol"
+export { schema } from "./schema"
